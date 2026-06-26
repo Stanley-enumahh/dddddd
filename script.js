fae31c1b4a5bdae91eeb5263a83be183ebc1b7f3
Stanley enumah <enumah44@gmail.com>
2026-06-26 11:05:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const hamburgerX = document.querySelector(".hamburgerX");
+const navList = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("nav-open");
+});
